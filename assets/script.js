9231275f8565fd580b8c29c1279f459448178a87
imbingz <bingz@me.com>
2020-08-25 18:11:53 -0600

    added pseudocodes

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// add event listener on serach button
+// - get the input city from user
+// - generate a new URL to make a call to query
+// - display the current weahter information in main display area (make sure to clear the display )
+// - display 5 day forecact (using a for loop)
+
+//Store search history to local storage 
+// display the last search when refresh the app 
+
+// add event listener on ul of search - hisroty -city list (use $(this)) )
+// - use data-city value as queryterm to make a call
+
+// /* BUILD QUERY URL 
