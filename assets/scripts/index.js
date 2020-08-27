265df34ba9bdc27b543ba133c1cae759245340b6
imbingz <bingz@me.com>
2020-08-27 16:45:50 -0600

    added local storage to current w display

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -36,96 +36,104 @@ $(document).ready(function() {
 	function displaySearchHistory(city) {
 		city = $('#cityInput').val().trim();
 
-		// First check if there is any user input
 		if (city) {
-			//Clear UL conent
 			$('.collection').empty();
 			//add city input to the start of the array
 			searchCity.unshift(city);
-			// delete duplication and only show 8 lates cities
-			var latestSearch = searchCity.slice(0, 8).filter(onlyUnique);
+			// delete duplication and only keep  9 latest searches
+			var latestSearch = searchCity.slice(0, 9).filter(onlyUnique);
 
 			// Set local storage to save latest search array
 			localStorage.setItem('latestSearch', JSON.stringify(latestSearch));
 
-			//Get localStorage data to display the saved city li
 			latestSearch = JSON.parse(localStorage.getItem('latestSearch'));
 
 			for (let i = 0; i < latestSearch.length; i++) {
-				// Append saved city li to ul
 				let sampleCityLi = `
-			    <li class="collection-item">
-			      <a href="#" class="grey-text text-darken-1" id="savedCity" data-city=${latestSearch[i]}>${latestSearch[i]}</a>
+			    <li class="collection-item grey-text text-darken-1" id="savedCity" data-city="${latestSearch[i]}">${latestSearch[
+					i
+				].toUpperCase()}
 			    </li>
-			    `;
+          `;
 				$('.collection').append(sampleCityLi);
-
-				//Add event handler on saved city li
-				$('#savedCity').on('click', function() {
-					let savedCity = $(this).attr('data-city');
-					let apiKey = '3019514fb26959aff7eeb1e73e5aa725';
-					$.ajax({
-						url: `https://api.openweathermap.org/data/2.5/weather?q=${savedCity}&units=imperial&appid=${apiKey}`,
-						method: 'GET'
-					}).then(updateCurrentWeather);
-					$.ajax({
-						url: `https://api.openweathermap.org/data/2.5/forecast?q=${savedCity}&units=imperial&appid=${apiKey}`,
-						method: 'GET'
-					}).then(updateForecastPage);
-				});
 			}
+			//Add event handler on saved city li
+			$('li').on('click', function() {
+				clear();
+				let savedCity = $(this).attr('data-city');
+				console.log(savedCity);
+				let apiKey = '3019514fb26959aff7eeb1e73e5aa725';
+				$.ajax({
+					url: `https://api.openweathermap.org/data/2.5/weather?q=${savedCity}&units=imperial&appid=${apiKey}`,
+					method: 'GET'
+				}).then(updateCurrentWeather);
+				$.ajax({
+					url: `https://api.openweathermap.org/data/2.5/forecast?q=${savedCity}&units=imperial&appid=${apiKey}`,
+					method: 'GET'
+				}).then(updateForecastPage);
+			});
 		}
 	}
 
-	//Current Weather Update
-
 	function updateCurrentWeather(cwData) {
 		var apiKey = '3019514fb26959aff7eeb1e73e5aa725';
-		let cityName = cwData.name;
-		let currentDate = moment().format('dddd, LLL');
+		// let cityName = cwData.name;
+		// let currentDate = moment().format('MM/DD/YYYY');
 		let iconCode = cwData.weather[0].icon;
-		let iconURL = 'http://openweathermap.org/img/w/' + iconCode + '.png';
-		let temp = cwData.main.temp;
-		let humidity = cwData.main.humidity;
-		let windSpeed = cwData.wind.speed;
+		// let iconURL = 'http://openweathermap.org/img/w/' + iconCode + '.png';
+		// let temp = cwData.main.temp;
+		// let humidity = cwData.main.humidity;
+		// let windSpeed = cwData.wind.speed;
 		let lon = cwData.coord.lon;
 		let lat = cwData.coord.lat;
+		// let id = cwData.sys.id
 
-		// Generate UV index query URL using lon and lat from current weather query response
 		let uvQueryURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
 
-		// Making query call to UV index data
 		$.ajax({
 			url: uvQueryURL,
 			mathod: 'GET'
 		}).then(function(uvData) {
+			let uvColor = '';
 			let uvIndex = uvData.value;
-			let uvColor;
 
-			// Color Label UV index
-			if (uvIndex <= 2) {
+			let query = {
+				city: cwData.name,
+				date: moment().format('MM/DD/YYYY'),
+				icon: 'http://openweathermap.org/img/w/' + iconCode + '.png',
+				temp: cwData.main.temp,
+				humidity: cwData.main.humidity,
+				windSpeed: cwData.wind.speed,
+				uv: uvIndex
+			};
+
+			if (query.uv <= 2) {
 				uvColor = 'green';
 				console.log(uvColor);
-			} else if (uvIndex <= 5) {
+			} else if (query.uv <= 5) {
 				uvColor = 'yellow';
-			} else if (uvIndex <= 7) {
+			} else if (query.uv <= 7) {
 				uvColor = 'orange';
-			} else if (uvIndex <= 10) {
+			} else if (query.uv <= 10) {
 				uvColor = 'red';
 			} else {
 				uvColor = 'voilet';
 			}
-			// Display Current Weather Condition
+
+			localStorage.setItem('query', JSON.stringify(query));
+
+			let lastQuery = JSON.parse(localStorage.getItem('query'));
+
 			let currentDay = `
       <div class="card card-panel current">
-				<h3 class="card-title" id='cityName'>${cityName}<span><img id="wicon" src="${iconURL}" alt="Weather icon"></span>	</h3>
-				<p id="date"><small> ${currentDate} </small></p> 
-        <p class="temperature">Temperature: <span id="temperature">${temp} &deg;F</span></p>
-        <p class="humidity">Humidity: <span id="humidity">${humidity}%</span></p>
-        <p class="windSpeed">Wind Speed: <span id="windSpeed">${windSpeed} MPH</span></p>
-        <p class="uv">UV Index: <span id="uv" class="${uvColor}">${uvIndex}</span></p>        
+        <h3 class="card-title" id='cityName'>${lastQuery.city} <span id="date"> ${lastQuery.date} </span> <img id="wicon" src="${lastQuery.icon}" alt="Weather icon"></h3>
+        <p class="temperature">Temperature: <span id="temperature">${lastQuery.temp} &deg;F</span></p>
+        <p class="humidity">Humidity: <span id="humidity">${lastQuery.humidity}%</span></p>
+        <p class="windSpeed">Wind Speed: <span id="windSpeed">${lastQuery.windSpeed} MPH</span></p>
+        <p class="uv">UV Index: <span id="uv" class="${uvColor} white-text">${lastQuery.uv}</span></p>        
       </div>
-    `;
+		`;
+
 			$('#main-display').append(currentDay);
 		});
 	}
