11e6dc08a5334316499574e5c3a4d2f409ff3aaf
imbingz <bingz@me.com>
2020-08-28 19:24:21 -0600

    used local storage to display last search

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -67,14 +67,10 @@ $(document).ready(function() {
 	}
 
 
-
-
-
 	/* UPDATE PAGE 
 	==================================================================================================== */
 
 	
-
 	// check and remove the duplication of searched cities 
 	function onlyUnique(value, index, self) {
 		return self.indexOf(value) === index;
@@ -98,11 +94,13 @@ $(document).ready(function() {
 
 	displayLocalStorage()
 	
-	// Use localStorage to display last search
+	// Use localStorage to display last search cities
 	function displayLocalStorage() {
 		let latestSearch = JSON.parse(localStorage.getItem('latestSearch'));
 		//Use for loop to dispaly saved search cites 
 		if (latestSearch) {
+			// delete duplication and only keep  9 latest searches	
+			latestSearch = latestSearch.slice(0, 2).filter(onlyUnique);
 			for (let i = 0; i < latestSearch.length; i++) {
 			let sampleCityLi = `
 			    <li class="collection-item grey-text text-darken-1" id="savedCity" data-city="${latestSearch[i]}">${latestSearch[
@@ -130,53 +128,131 @@ $(document).ready(function() {
 				.catch(function(err) {
 				console.log(err);
 			});
-			// $.ajax({
-			// 	url: `https://api.openweathermap.org/data/2.5/forecast?q=${savedCity}&units=imperial&appid=${apiKey}`,
-			// 	method: 'GET'
-			// }).then(updateForecastPage).catch(function(err) {
-				// console.log(err);
-			// });
+			$.ajax({
+				url: `https://api.openweathermap.org/data/2.5/forecast?q=${savedCity}&units=imperial&appid=${apiKey}`,
+				method: 'GET'
+			}).then(updateForecastPage)
+				.catch(function(err) {
+				console.log(err);
+			});
 		});
 		}	
 	}
 
-
-//Current Weather Update
-
-function updateCurrentWeather(cwData) {
-	var apiKey = '3019514fb26959aff7eeb1e73e5aa725';
-	let cityName = cwData.name;
-	let currentDate = moment().format('dddd, LLL');
-	let iconCode = cwData.weather[0].icon;
-	let iconURL = 'http://openweathermap.org/img/w/' + iconCode + '.png';
-	let temp = cwData.main.temp;
-	let humidity = cwData.main.humidity;
-	let windSpeed = cwData.wind.speed;
-	let lon = cwData.coord.lon;
-	let lat = cwData.coord.lat;
-
-	let uvQueryURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
- 
-	// Making Query Call to UV Index API 
-	$.ajax({
-		url: uvQueryURL,
-		mathod: 'GET'
-	}).then(function(uvData) {
-		let uvIndex = uvData.value;
-		let currentDay = `
-      <div class="card card-panel current">
-				<h3 class="card-title" id='cityName'>${cityName}<span><img id="wicon" src="${iconURL}" alt="Weather icon"></span>	</h3>
-				<p id="date"><small> ${currentDate} </small></p> 
-        <p class="temperature">Temperature: <span id="temperature">${temp} &deg;F</span></p>
-        <p class="humidity">Humidity: <span id="humidity">${humidity}%</span></p>
-        <p class="windSpeed">Wind Speed: <span id="windSpeed">${windSpeed} MPH</span></p>
-        <p class="uv">UV Index: <span id="uv" class="red white-text">${uvIndex}</span></p>        
-      </div>
-    `;
-		$('#main-display').append(currentDay);
-	});
-}
 	
+	
+	
+function updateCurrentWeather(cwData) {
+		let iconCode = cwData.weather[0].icon;
+		let lon = cwData.coord.lon;
+		let lat = cwData.coord.lat;
+		// Generate UI Index API URL 
+		let uvQueryURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
+		//Making query call to get ui idenx
+		$.ajax({
+			url: uvQueryURL,
+			mathod: 'GET'
+		}).then(function(uvData) {
+			
+			let uvIndex = uvData.value;
+
+			let current = {
+				city: cwData.name,
+				date: moment().format('ddd, LL'),
+				icon: 'http://openweathermap.org/img/w/' + iconCode + '.png',
+				temp: cwData.main.temp,
+				humidity: cwData.main.humidity,
+				windSpeed: cwData.wind.speed,
+				uv: uvIndex
+			};
+
+			// Check ui index and color label them
+			if (current.uv <= 2) {
+				let uvColor = '';
+				uvColor = 'green';
+				console.log(uvColor);
+			} else if (current.uv <= 5) {
+				uvColor = 'yellow';
+			} else if (current.uv <= 7) {
+				uvColor = 'orange';
+			} else if (current.uv <= 10) {
+				uvColor = 'red';
+			} else {
+				uvColor = 'voilet';
+			}
+		
+			let currentDay = `
+      <div class="card card-panel current">
+				<h3 class="card-title" id='cityName'>${current.city}</h3>
+				<P id="date">${current.date}</p> 
+				<div><img id="wicon" src="${current.icon}" alt="Weather icon"></div>
+        <p class="temperature">Temperature: <span id="temperature">${current.temp} &deg;F</span></p>
+        <p class="humidity">Humidity: <span id="humidity">${current.humidity}%</span></p>
+        <p class="windSpeed">Wind Speed: <span id="windSpeed">${current.windSpeed} MPH</span></p>
+        <p class="uv">UV Index: <span id="uv" class="${uvColor} white-text">${current.uv}</span></p>        
+      </div>
+		`;
+			$('#main-display').append(currentDay);
+	})
+	}
+	
+	displayStoredCurrent()
+
+	// Use localStorage to display last searched city
+	function displayStoredCurrent() {
+		let latestSearch = JSON.parse(localStorage.getItem('latestSearch'));
+		if (latestSearch !== null) {
+			searchCity[0] = latestSearch[0] 
+			currentQueryCall()
+		}
+	}
+	
+ // 5-Day Weather Forecast 
+	
+	function updateForecastPage(wfData) {
+		for (let i = 1; i < wfData.list.length; i += 8) {
+			// Get info from response data
+			let year = wfData.list[i].dt_txt.slice(0, 4);
+			let month = wfData.list[i].dt_txt.slice(5, 7);
+			let day = wfData.list[i].dt_txt.slice(8, 10);
+			let dates = month + '/' + day + '/' + year;
+			let iconCode = wfData.list[i].weather[0].icon;
+			let iconURL = 'http://openweathermap.org/img/w/' + iconCode + '.png';
+			let temp = wfData.list[i].main.temp;
+			let humidity = wfData.list[i].main.humidity;
+
+			// Set up HTML structure
+			let forecastDay = `
+			<div class="col s12 l5ths">
+				<div class="card card-panel forecast blue accent-4">
+					<h4 class="card-title white-text">${dates}</h4>
+					<div class="icon">
+						<img id="wicon" src="${iconURL}" alt="Weather icon">
+					</div>
+					<p class="temperature white-text">
+						Temperature: <span id="temperature">${temp} &deg;F</span>
+					</p>
+					<p class="humidity white-text">
+						Humidity: <span id="humidity">${humidity}%</span>
+					</p>
+				</div>
+			</div>
+		`;
+			// Display 5- day weather forecast
+			$('#forecast-div').append(forecastDay);
+		}
+	}
+
+	displayStoredForecast() 
+	
+	// Use localStorage to display last searched city
+	function displayStoredForecast() {
+		let latestSearch = JSON.parse(localStorage.getItem('latestSearch'));
+		if (latestSearch !== null) {
+			searchCity[0] = latestSearch[0] 
+			forecastQueryCall()
+		}
+	}
 	
 /* WARNING MESSAGES ===================================================================================================================================*/
 	function noInputWarning() {
@@ -207,9 +283,11 @@ function updateCurrentWeather(cwData) {
 			clear();
 			displaySearchHistory();
 			currentQueryCall()
-
+			forecastQueryCall()
 		} else {
 			noInputWarning();
 		}
 	});
+
+
 });
